df5bacb13a5074ded98a5fdc814260948b7d9731
marianux <llamedom@gmail.com>
2016-12-02 16:30:10 -0300

    disable error output in ADC2units.m

--- common/ADC2units.m
+++ common/ADC2units.m
@@ -65,14 +65,14 @@ end
 
 if( isempty(origin_units_idx) )
 %     error('ADC2units:UnknownUnits', ['Unknown units: ' header.units(1,:) ]);
-    fprintf(2, 'Unknown units: %s\n', header.units(1,:) );
+%     fprintf(2, 'Unknown units: %s\n', header.units(1,:) );
     new_header = header;
     return
 end
 
 if( isempty(target_units_idx) )
 %     error('ADC2units:UnknownUnits', ['Unknown units: ' target_units ]);
-    fprintf(2, ['Unknown units: %s\n', target_units]);
+%     fprintf(2, ['Unknown units: %s\n', target_units]);
     new_header = header;
     return
 end
