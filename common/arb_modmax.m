d71aba74d790722a6497f86787f27abde3530f44
marianux <llamedom@gmail.com>
2017-04-13 10:06:57 -0300

    Cambios en la interfaz de common/ECGtask_arbitrary_function.m

---
+++ common/arb_modmax.m
@@ -0,0 +1,20 @@
+% Description:
+% 
+% An arbitrary task version of the modmax function for using with ECG
+% wrapper objects% 
+% 
+% See also ECGtask_arbitrary_function
+% 
+% 
+% Author: Mariano Llamedo Soria (llamedom at frba.utn.edu.ar)
+% Version: 0.1 beta
+% Birthdate  : 5/4/2017
+% Last update: 5/4/2017
+% Copyright 2008-2017
+% 
+function payload = arb_modmax( ECG_matrix, ECG_header, ECG_start_offset, progress_handle, payload_in)
+
+aux_modmax = modmax(ECG_matrix, 1, payload_in.thr, 1, round(payload_in.detection_threshold*ECG_header.freq) );
+
+payload = colvec(aux_modmax + ECG_start_offset - 1);
+
---
+++ common/default_start_function.m
@@ -0,0 +1,23 @@
+% 
+% Description:
+% 
+% The default behavior for an arbitrary function
+% (ECGtask_arbitrary_function) of the strat function is to do nothing. 
+% 
+% 
+% See also default_finish_function, ECGtask_arbitrary_function
+% 
+% Author: Mariano Llamedo Soria (llamedom at frba.utn.edu.ar)
+% Version: 0.1 beta
+% Birthdate  : 17/4/2015
+% Last update: 17/4/2015
+% Copyright 2008-2017
+% 
+function bStarted = default_start_function(obj, ECG_header)
+
+% data used to share initial/previous conditions among data segments or
+% iterations.
+obj.payload.interproc_data = [];
+
+% Flag to indicate correct initialization.
+bStarted = true;
